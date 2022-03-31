d2f4d14f48b7b5e7ef88aa1c6b710b830367a69c
beccajules <gpavuluri.git@gmail.com>
2022-03-31 11:00:04 +0530

    DummyCommit

--- src/main/java/org/gitsample/Company.java
+++ src/main/java/org/gitsample/Company.java
@@ -2,22 +2,20 @@ package org.gitsample;
 
 public class Company {
 	
-
-	//Change 2
-
-	//Change 1
-	
-	//Change 3
-
-
 	private String name;
 	private int salary;
+	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	//Just a comment!
+	private int getSalary() {
+		return salary;
+	}
+	private void setSalary(int salary) {
+		this.salary = salary;
+	}
 	
 }
